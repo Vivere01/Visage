1eb6112b33aff3a2bb8ce53eed3eac176ccb4218
Matheus Abreu <matheusoliveiraabreu3@gmail.com>
2026-04-27 13:01:49 -0300

    fix(prisma): fix type error in seed.ts by using default prisma constructor

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,13 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 
 import "dotenv/config"
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-})
+const prisma = new PrismaClient()
 
 async function main() {
   const hashedPassword = await bcrypt.hash('admin123', 10)
